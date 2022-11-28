a52e0931493def8159aeb4568a91b36a3d573912
s486790 <s486790@FXHC140M06.ocsb.local>
2022-11-28 12:17:39 -0500

    submit the Q1Volume.py-Unit4

---
+++ Unit 4/Q1Volume.py
@@ -0,0 +1,15 @@
+# volume of a Rectangular prism 
+length = 0
+width = 0
+height = 0
+volume = 0
+
+
+
+lenght = input("what is the lenth: ")
+width = input("what is the width: ")
+height = input("what is the height: ")
+
+volume = (int(length) * int(width) * int(height))
+
+print("volume of a rectangular prism with dimensions of " + str(lenght) + ","  +  str(width) + ", and " + str(height) + " is " + str(volume) + ",")
