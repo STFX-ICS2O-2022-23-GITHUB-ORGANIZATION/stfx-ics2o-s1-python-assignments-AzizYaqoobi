a225ee58d057aa05d5bbfc730980c973cd106ca0
s486790 <s486790@FXHC140M06.ocsb.local>
2022-12-13 12:50:29 -0500

    lesson

---
+++ Unit 5/Boolen.py
@@ -0,0 +1,22 @@
+
+if (1 == 1):
+    
+    print("hello world")
+    
+    
+if (2 == 2):
+    
+    print("hello world")
+    
+    
+if (1 > 2):
+    
+    print("hello world")
+    
+if  (1 == 1) or (1 > 2): 
+    print("AND WORED")
+    
+    
+    
+if not (1 == 2):
+    print("Hello")
---
+++ Unit 5/ForLoop.py
@@ -0,0 +1,26 @@
+#for i in range (5, 10):
+    
+    
+ #   print(i)
+    
+    
+#ROWS = 10
+
+#for i in range (0, ROWS):
+  #  blanks = " " * i 
+   # print(blanks + '*')
+
+
+import time
+
+
+ROWS = 10  
+CHARS = 10 
+
+for i in range (0, ROWS): 
+    for j in range (0, CHARS): 
+        
+        print("*", end = "")
+        time.sleep(.5)
+        
+    print("")
