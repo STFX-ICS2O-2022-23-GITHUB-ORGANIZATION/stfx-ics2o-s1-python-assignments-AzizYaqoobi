830c1851d58d5d5fc9324aba9e4ce385a743a55c
s486790 <s486790@FXHC140M06.ocsb.local>
2023-01-11 12:28:02 -0500

    done

--- Unit 4/Q1Counting.py
+++ Unit 4/Q1Counting.py
@@ -1,10 +1,19 @@
+# NAME OF AUTHOR:  Azizullah Yaqoobi 
+# NAME OF THE PROGRAM:  Q1Counting 
+# DATE OF CREATION: 2022 
+# PURPOSE OF PROGRAM:  
+# OUTPUT numbers 
+
+
+
+# VARIABLE DEFINITION
 num = 0 
 num2 = 0
-
+# INPUT
 num = int(input("Enter one large number: "))
 num2 = int(input("Enter the number you want to count by: "))
 
-
+# PROCESSING
 for num in range (num, 0, -num2):
     
     print(num)
