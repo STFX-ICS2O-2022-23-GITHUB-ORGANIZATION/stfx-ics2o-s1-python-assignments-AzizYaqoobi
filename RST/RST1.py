5732ed70be317c7df9ccd5cb894f99b9791743e6
s486790 <s486790@FXHC140M06.ocsb.local>
2023-01-19 12:02:14 -0500

    done the RST

--- RST/RST1.py
+++ RST/RST1.py
@@ -1,15 +1,24 @@
+# NAME OF AUTHOR: Azizullah Yaqoobi 
+# NAME OF THE PROGRAM: Rock, paper, and scissors game  
+# DATE OF CREATION: 2023, 01, 18
+# PURPOSE OF PROGRAM: to play the rock, paper and scissors game alone. 
+
+
 
 import random
 while True:
+    # VARIABLE DEFINITION
     options = ("rock", "paper", "scissors")
     player = None
+     # Generate a random choice from the options 
     computer = random.choice(options)
+    # INPUT
     while player not in options:
         player = input("Enter a choice (rock, paper, scissors): ")
-
+    # OUTPUT
     print(f"player: {player}")
     print(f"computer: {computer}: ")
-
+    # PROCESSING
     if player == computer:
         print("It's a tie!")
     elif player == "rock" and computer == "scissors":
--- Unit 4/activity6.py
+++ Unit 4/activity6.py
@@ -3,34 +3,40 @@
 # DATE OF CREATION: 2023. 01. 16 
 # PURPOSE OF PROGRAM: engaging the number guessing game 
 
-num1 = 0
-num2 = 0
-num3 = 0
-
-num1 = int(input("Enter your first number: "))
-num2 = int(input("Enter your first number: "))
-
-import random
-
-# Generate a random number between 1 and 100
-n = random.randint(num1, num2)
-
-# Keep track of the number of guesses
-var = 0
-
 while True:
-    # Get the user's guess
-    num3 = int(input("Guess a number between 1 and 100: "))
-
-    # Increase the number of guesses
-    var += 1
-
-    # Check if the guess is correct
-    if num3 == n:
-        print("Congratulations! You guessed the number in", var, "guesses!")
-        break
-    elif num3 < n:
-        print("Too low!")
-    else:
-        print("Too high!")
-    
+  num1 = 0
+  num2 = 0
+  num3 = 0
+ 
+  num1 = int(input("Enter your first number: "))
+  num2 = int(input("Enter your first number: "))
+ 
+  import random
+ 
+  # Generate a random number between
+  n = random.randint(num1, num2)
+ 
+  # Keep track of the number of guesses
+  var = 0
+ 
+  while True:
+      # Get the user's guess
+      num3 = int(input("Guess a number between the two number you entered: "))
+ 
+      # Increase the number of guesses
+      var += 1
+ 
+      # Check if the guess is correct
+      if num3 == n:
+          print("Congratulations! You guessed the number in", var, "guesses!")
+          break
+      elif num3 < n:
+          print("Too low!")
+      else:
+          print("Too high!")
+  play_again = input("Do you want to play again? (yes/no)")
+  if play_again.lower() == "yes":
+        continue
+  else:
+      print("Thank you for playing!")
+      break
